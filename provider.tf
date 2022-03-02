371170eec1e9b192bc349295d1a9c223dec4e00e
Soner Gunay <soner.gunay@agilisys.co.uk>
2022-03-02 10:13:50 +0000

    profile changed

---
+++ provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    profile = "aws"
+  region = "us-east-1"
+
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.3.0"
+    }
+  }
+}
+
+
