91b5629c0f2fae156e15d20b15cfcdc827de926a
Soner Gunay <soner.gunay@agilisys.co.uk>
2022-05-08 20:53:03 +0100

    allupdated

--- Group1/sg.tf
+++ Group1/sg.tf
@@ -9,4 +9,18 @@ resource "aws_security_group" "allow_ssh" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
