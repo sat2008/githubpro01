41cecc109e2a661ddd37852cf59649a3c4a25152
Soner Gunay <soner.gunay@agilisys.co.uk>
2022-05-08 20:52:15 +0100

    sg groups added

--- EKS/internet-gateway.tf
+++ EKS/internet-gateway.tf
@@ -5,3 +5,15 @@ resource "aws_internet_gateway" "main" {
     Name = "main"
   }
 }
+
+# Route table: attach Internet Gateway 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.gr_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terra_igw.id
+  }
+  tags = {
+    Name = "publicRouteTable"
+  }
+}
